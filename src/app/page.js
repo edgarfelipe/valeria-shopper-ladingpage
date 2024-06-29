946655473cfda8def91030ccfe4cd9411ee56c0e
Edgar Fernandes <92003953+edgarfelipe@users.noreply.github.com>
2024-06-29 16:18:28 +0100

    main

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Gallery from './components/Gallery';
@@ -11,20 +12,27 @@ import PhotoSlideshow from './components/PhotoSlideshow'; // Importe o component
 import BrandGallery from './components/client/BrandGallery';
 
 const Home = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  const isMobileQuery = useMediaQuery({ query: '(max-width: 768px)' });
+
+  useEffect(() => {
+    setIsMobile(isMobileQuery);
+  }, [isMobileQuery]);
+
   return (
     <div className="bg-gray-50 min-h-screen">
       <Header />
       <main className="container mx-auto p-4">
         <section className="intro text-center p-4 bg-cover bg-center" style={{ backgroundImage: "url('/images/108.jpg')" }}>
           <div className="bg-black bg-opacity-50 p-8 rounded-lg">
-            <h1 className="text-5xl font-bold text-white mb-4">VALÉRIA</h1>
+            {!isMobile && <h1 className="text-5xl font-bold text-white mb-4">VALÉRIA</h1>}
             <p className="text-lg text-white">
               Desde muito jovem, me fascinava o mundo das marcas de luxo, o estilo e o bom gosto...
             </p>
           </div>
         </section>
         <section className="gallery mt-8">
-          <h2 className="text-center text-3xl font-bold mb-4">Marcas</h2>
+          <h2 className="text-center text-3xl font-bold mb-4">Galeria de Marcas</h2>
           <BrandGallery />
         </section>
         <PersonalShopper />
