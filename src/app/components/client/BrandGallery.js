2f944c7cb31caf0a8694307299bc623adae3efaf
Edgar Fernandes <92003953+edgarfelipe@users.noreply.github.com>
2024-06-30 11:42:25 +0100

    main

--- src/app/components/client/BrandGallery.js
+++ src/app/components/client/BrandGallery.js
@@ -4,19 +4,19 @@ import React, { useState } from 'react';
 import ProductModal from '../ProductModal';
 
 const brands = [
-  { name: 'Prada', image: '/images/prada.jpg' },
-  { name: 'Dior', image: '/images/dior.jpg' },
-  { name: 'Gucci', image: '/images/gucci.jpg' },
-  { name: 'LV', image: '/images/lv.jpg' },
-  { name: 'Loewe', image: '/images/loewe.jpg' },
-  { name: 'Saint Laurent', image: '/images/saint-laurent.jpg' },
-  { name: 'Maison Alaia', image: '/images/maison-alaia.jpg' },
-  { name: 'Valentino', image: '/images/valentino.jpg' },
-  { name: 'Cult Gaia', image: '/images/cult-gaia.jpg' },
-  { name: 'Celine', image: '/images/celine.jpg' },
-  { name: 'Balenciaga', image: '/images/balenciaga.jpg' },
-  { name: 'Jacquemus', image: '/images/jacquemus.jpg' },
-  { name: 'MIU MIU', image: '/images/miu-miu.jpg' },
+  { name: '', image: '/images/prada.png' },
+  { name: '', image: '/images/dior.png' },
+  { name: '', image: '/images/gucci.png' },
+  { name: '', image: '/images/lv.png' },
+  { name: '', image: '/images/loewe.png' },
+  { name: '', image: '/images/saint-laurent.png' },
+  { name: '', image: '/images/maison-Alaia.jpg' },
+  { name: '', image: '/images/valentino.png' },
+  { name: '', image: '/images/cult-gaia.png' },
+  { name: '', image: '/images/celine.png' },
+  { name: '', image: '/images/balenciaga.png' },
+  { name: '', image: '/images/jacquemus.png' },
+  { name: '', image: '/images/miu-miu.png' },
 ];
 
 const BrandGallery = () => {
@@ -39,10 +39,10 @@ const BrandGallery = () => {
         {brands.map((brand, index) => (
           <div
             key={index}
-            className="brand bg-white p-4 shadow rounded-lg cursor-pointer transform transition duration-300 hover:scale-105 hover:shadow-lg"
+            className="brand flex flex-col items-center justify-center bg-white p-4 shadow rounded-lg cursor-pointer transform transition duration-300 hover:scale-105 hover:shadow-lg"
             onClick={() => handleBrandClick(brand.name)}
           >
-            <img src={brand.image} alt={brand.name} className="w-full h-auto rounded" />
+            <img src={brand.image} alt={brand.name} className="w-auto h-24" />
             <p className="text-center mt-2 text-lg font-semibold">{brand.name}</p>
           </div>
         ))}
